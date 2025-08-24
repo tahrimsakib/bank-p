22f72c4b0391fc238892174f284eac32d19e1a34
tahrim sakib <you@example.comdmsakib1122@gmail.com>
2025-08-24 20:09:53 +0600

    added js file and home js file

---
+++ js file/home.js
@@ -0,0 +1,6 @@
+  document
+        .getElementById("add-money-btn")
+        .addEventListener("click", function (ev) {
+          ev.preventDefault();
+          console.log("Button clicked!");
+        });
---
+++ js file/script.js
@@ -0,0 +1,15 @@
+document.getElementById("login-button").addEventListener("click", function (e) {
+  e.preventDefault(); //eita dite hbe na hoi page load hoye jabe
+  const num = 12345678910;
+  const pin = 1234;
+
+  const numCheck = parseInt(document.getElementById("login-num").value);
+
+  const pinCheck = parseInt(document.getElementById("login-pin").value);
+
+  if (numCheck === num && pinCheck === pin) {
+    window.location.href = "./home.html";
+  } else {
+    alert("Please give us right number and pin");
+  }
+});
