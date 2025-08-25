2a24a2ae033058add12bcffb67f48719f07abd77
tahrim sakib <you@example.comdmsakib1122@gmail.com>
2025-08-25 14:30:27 +0600

    added cashout

--- home.js
+++ home.js
@@ -1,4 +1,4 @@
-pinNumber = 1234
+pinNumber = 1234;
 
 document
   .getElementById("add-money-btn")
@@ -8,16 +8,16 @@ document
     const addToAmount = parseInt(document.getElementById("add-money").value);
 
     const bankAccountNum = document.getElementById("bank-account-num").value;
-    const bankAcTo =parseInt(bankAccountNum)
+    const bankAcTo = parseInt(bankAccountNum);
     if (bankAccountNum.length < 11) {
       alert("ki dios sob");
       return;
     }
     const accountPin = parseInt(document.getElementById("account-pin").value);
 
-    if(accountPin !== pinNumber){
-      alert('pin daw')
-      return
+    if (accountPin !== pinNumber) {
+      alert("pin daw");
+      return;
     }
 
     const currentAmountnow = parseInt(
@@ -29,9 +29,26 @@ document
     document.getElementById("currentAmount").innerText = sum;
   });
 
+document.getElementById("cash-out-btn").addEventListener("click", function (e) {
+  e.preventDefault();
+  const AgentNumber = document.getElementById("Agent-Number").value;
 
-  document.getElementById('cash-out-btn').addEventListener('click', function(e){
-    e.preventDefault()
+  const AgentNumberP = parseInt(AgentNumber);
 
-    
-  })
+  if (AgentNumber.length < 11) {
+    alert("give me currect number");
+    return;
+  }
+
+  const accountPin = parseInt(document.getElementById("account-pin-num").value);
+
+  if (accountPin !== pinNumber) {
+    alert("Wrong PIN");
+    return;
+  }
+
+  const amountValue = document.getElementById("amount").value;
+  const currentAmount = document.getElementById("currentAmount").innerText;
+  const minus = currentAmount - amountValue;
+  document.getElementById("currentAmount").innerText = minus;
+});
